19bef08f0c41ecb6b4845dea4707ccca0a8c1097
Robin Diddams <robindiddams@gmail.com>
2023-12-19 16:07:54 -0600

    rework aliases alias vi to vim create alias file as the user instead of root dont write to the sudoers file twice

--- personalize.sh
+++ personalize.sh
@@ -3,13 +3,25 @@ set -euxo pipefail
 
 USER="robindiddams"
 HOME="/home/$USER"
+ALIAS_FILE="$HOME/.bash_aliases"
 
 apt update && apt install htop curl vim figlet
 mkdir -p $HOME/.ssh
 curl https://github.com/$USER.keys >> $HOME/.ssh/authorized_keys
-echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# check if this string is already in the sudoers file
+if ! grep -q "$USER ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+	echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
 hostname | figlet -f smslant > /etc/motd
 
-echo "alias ll=\"ls -la\"" >> $HOME/.bash_aliases
+# check if bash aliases exists and if not create it as user
+if [ ! -f $ALIAS_FILE ]; then
+	touch $ALIAS_FILE
+	chown $USER:$USER $ALIAS_FILE
+fi
 
-
+# aliases as a multiline string
+cat << EOF >> $ALIAS_FILE
+alias ll="ls -la"
+alias vi="vim"
+EOF

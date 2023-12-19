7fd9787073f64b3b5e527a831f44d25d1b2f42c5
Robin Diddams <robindiddams@gmail.com>
2023-12-18 20:28:45 -0600

    rework to be run as root

--- personalize.sh
+++ personalize.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+set -euxo pipefail
 
-sudo apt update && sudo apt install htop curl vim
+USER="robindiddams"
+HOME="/home/$USER"
+
+apt update && apt install htop curl vim figlet
 mkdir -p $HOME/.ssh
-curl https://github.com/robindiddams.keys >> $HOME/.ssh/authorized_keys
+curl https://github.com/$USER.keys >> $HOME/.ssh/authorized_keys
+echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+hostname | figlet -f smslant > /etc/motd
+
+echo "alias ll=\"ls -la\"" >> $HOME/.bash_aliases
+
 

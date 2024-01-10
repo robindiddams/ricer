b9492806f2d33f1e9fd332aab8b85050d62eab1b
Robin Diddams <robindiddams@gmail.com>
2024-01-10 15:14:52 -0600

    use -y on apt, check for authorized_keys before creating it 🔑

--- personalize.sh
+++ personalize.sh
@@ -5,13 +5,22 @@ USER="robindiddams"
 HOME="/home/$USER"
 ALIAS_FILE="$HOME/.bash_aliases"
 
-apt update && apt install htop curl vim figlet
+apt update && apt install -y htop curl vim figlet
 mkdir -p $HOME/.ssh
+
+# check if authorized_keys exists and if not create it as user
+if [ ! -f $HOME/.ssh/authorized_keys ]; then
+	touch $HOME/.ssh/authorized_keys
+	chown $USER:$USER $HOME/.ssh/authorized_keys
+fi
 curl https://github.com/$USER.keys >> $HOME/.ssh/authorized_keys
+
 # check if this string is already in the sudoers file
 if ! grep -q "$USER ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
 	echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 fi
+
+# set the motd
 hostname | figlet -f smslant > /etc/motd
 
 # check if bash aliases exists and if not create it as user
